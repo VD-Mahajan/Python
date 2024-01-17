ecc9b4c3fee93cf0840d25303b4990c2f7743299
Vishal Mahajan <mahajanvishal410a@gmail.com>
2024-01-17 22:05:10 +0530

    Initial commit

---
+++ Datatypes/Program01.py
@@ -0,0 +1,45 @@
+a=10
+print(a)        #=======> 10
+
+#In python2 we had long to store large value but in python3 int can store large values stored by long
+
+#Decimal form
+a=5
+print(a)        #=======>5
+
+#Octal form
+a=0o12
+print(a)        #=======>10
+
+#Octal to decimal
+b="1177"
+octal_to_decimal=int(b,8)
+print(octal_to_decimal)            #=======>639
+
+#Binary form
+a=0B0001101
+print(a)                      #=======>13
+
+#Hexadecimal form
+a=0xff
+print(a)                       #=======>255
+
+#int a = 56     not valid
+a:int =56      # valid but not neccesary
+print(a)                       #=======>56
+
+int = 19
+print(int)                     #=======>19
+
+str="Vishal Mahajan"
+print(str)                     #=======>Vishal Mahajan
+
+f=1.2e1       
+print(f)                       #=======>12.0
+
+a=True
+print(a)                       #========>True
+
+print(True+True)               #========>2
+print(False+True)              #========>1
+print(False+False)             #========>0
