b8d999916981c3bc5ef9e41fec2a1777047dd3c3
Vishal Mahajan <mahajanvishal410a@gmail.com>
2024-01-18 21:25:09 +0530

    Initial commit

--- TypeCasting/Program01.py
+++ TypeCasting/Program01.py
@@ -6,3 +6,17 @@ print(int(a))
 #int to float
 a=2
 print(float(a))
+
+#bool to int
+a=False
+print(int(a))
+
+#string to int
+a="32"
+print(int(a))
+
+#complex to int
+a=3+4j
+print(type(a))
+#print(int(a))      invalid
+
