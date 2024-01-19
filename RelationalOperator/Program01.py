ce801adc5acd180b29d3a48ef68251b5e1b31ecb
Vishal Mahajan <mahajanvishal410a@gmail.com>
2024-01-19 22:09:56 +0530

    Initial commit

---
+++ RelationalOperator/Program01.py
@@ -0,0 +1,23 @@
+x=10
+y=20
+
+print(x<y)              #True
+print(x>y)              #False
+print(x<=y)             #True
+print(x>=y)             #False
+print(x==y)             #False
+print(x!=y)             #True
+
+a=10
+b=20
+
+print('========')
+
+print(a and b)          #True and True===>20
+print(a or b)           #True or True===>10
+print(not a)            #not True===> False
+print(not 0)            #not False==> True
+print(0 and 50)         #False and True
+
+
+
