b5e453c7fa1399d8d9964f28e4d7180c39058433
Vishal Mahajan <mahajanvishal410a@gmail.com>
2024-01-19 21:50:42 +0530

    Initial commit

---
+++ ArithmeticOperator/Program01.py
@@ -0,0 +1,10 @@
+x=10
+y=20
+
+res=x++y                    #ignores extra + sign
+
+#print(x++)                 invalid syntax
+
+print(++x)                  #ignores ++ sign
+
+print(+++++++++++res)       #ignores extra + sign
