0690b3ace8b9216b4bf8c7aaa7507a322dc72679
Vishal Mahajan <mahajanvishal410a@gmail.com>
2024-01-20 21:20:22 +0530

    Initial commit

---
+++ Input/Program03.py
@@ -0,0 +1,14 @@
+x = 10
+y = 20
+
+print(x+y)
+
+a = input("enter value of a: ")
+b = input("enter value of b: ")
+
+print(a+b)
+
+a = int(input("enter value of a: "))
+b = int(input("enter value of b: "))
+
+print(a+b)
