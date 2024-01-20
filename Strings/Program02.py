7540be73e93078f530091e6b2a2832a3e522a113
Vishal Mahajan <mahajanvishal410a@gmail.com>
2024-01-20 21:26:13 +0530

    Initial commit

---
+++ Strings/Program02.py
@@ -0,0 +1,20 @@
+#type 2
+print('Mahajan')
+print("Mahajan")
+print('''Mahajan''')
+
+print("Vishal"+"Mahajan")
+print("Vishal","Mahajan")
+print(3*"Vishal Mahajan")
+
+
+
+#op==>
+'''
+Mahajan
+Mahajan
+Mahajan
+VishalMahajan
+Vishal Mahajan
+Vishal MahajanVishal MahajanVishal Mahajan
+'''
