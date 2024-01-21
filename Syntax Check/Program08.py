bdf1e4084ebd7a8c7e780b761031a327e99ce556
Vishal Mahajan <mahajanvishal410a@gmail.com>
2024-01-21 09:06:17 +0530

    Initial commit

---
+++ Syntax Check/Program08.py
@@ -0,0 +1,8 @@
+num=int(input("Enter number: "))
+
+if(num>0):
+    print("{} Number is positive number".format(num))
+elif(num<0):
+    print("{} Number is negative number".format(num))
+else:
+    print("{} Number is zero".format(num))
