a0294d3a82b44bb71289e73b9f3efa1b5ee697ea
Vishal Mahajan <mahajanvishal410a@gmail.com>
2024-01-21 08:47:46 +0530

    Initial commit

---
+++ Syntax Check/Program03.py
@@ -0,0 +1,21 @@
+age=int(input("Enter you age: "))
+
+if(age>=18):
+    pass
+print("Out of if")
+
+
+'''
+we use pass to indicate that empty block (nothing)
+
+we can use it to show un implemented part 
+
+eg.
+
+def my_function():
+    pass
+    
+class MyClass:
+    pass
+    
+'''
