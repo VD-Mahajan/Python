2fbd149778cae3c14fc475fa2f4265ecb4d86ff9
Vishal Mahajan <mahajanvishal410a@gmail.com>
2024-01-21 08:27:30 +0530

    Initial commit

--- Strings/Program04.py
+++ Strings/Program04.py
@@ -1,4 +1,4 @@
-# type4
+# type 4
 # print(formatted output)
 
 x = 10
@@ -9,3 +9,9 @@ print("Value of x: %d "%x)
 print("Value of x: %i "%x)
 print("Value of y: %f "%y)
 print("Value of a: %s "%a)
+
+c=complex(x,y)
+
+print("Value of c : %s"%c)
+
+print("Value of x={} and Value of y={} ".format(x,y))
