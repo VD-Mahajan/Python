93482160c0d5159c2984af271075ebc5756d7060
Vishal Mahajan <mahajanvishal410a@gmail.com>
2024-01-21 08:56:18 +0530

    Initial commit

---
+++ Syntax Check/Program06.py
@@ -0,0 +1,12 @@
+age=int(input("Enter age : "))
+
+if(age>=18):
+    print("Eligible")
+    {
+        print("Inside if")
+    }
+else:
+    print("Not Eligible")
+    {
+        print("Inside else")
+    }
