245ea8d59dccb185aa7f9ce5ae48d965b0281e59
Vishal Mahajan <mahajanvishal410a@gmail.com>
2024-01-22 22:31:22 +0530

    Initial commit

---
+++ Function/Program09.py
@@ -0,0 +1,12 @@
+class Alphabet:
+    def __init__(self):
+        print("In Parent Constructor")
+    def fun(self):
+        print("In fun of parent")
+
+class Google(Alphabet):
+    pass
+
+Alphabet()
+
+        
---
+++ Function/Program10.py
@@ -0,0 +1,10 @@
+class Alphabet:
+    def __init__(self):
+        print("In Parent Constructor")
+    def fun(self):
+        print("In fun of parent")
+
+class Google(Alphabet):
+    pass
+
+Google()
