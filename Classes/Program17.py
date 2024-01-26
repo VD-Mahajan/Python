19c337d6629871c85563721bc6f89a3ece5ab801
Vishal Mahajan <mahajanvishal410a@gmail.com>
2024-01-26 21:35:49 +0530

    Initial commit

---
+++ Classes/Program17.py
@@ -0,0 +1,11 @@
+class Test:
+    def __init__(self):
+        self.a=10
+        self.b=20
+    def m1(self):
+        self.c=30
+
+t = Test()
+print(t.c)          # AttributeError: 'Test' object has no attribute 'c'
+t.m1()
+print(t.c)
