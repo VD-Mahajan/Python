2d1758496e8b4723c94e7ca735598b330ed85c1f
Vishal Mahajan <mahajanvishal410a@gmail.com>
2024-01-30 15:29:44 +0530

    Initial commit

---
+++ Function/Program12.py
@@ -0,0 +1,13 @@
+def x(name):
+    if(name == "Kang"):
+        print("Hey Kang You are bad!")
+    else:
+        print("Hello",name,"Good morning")
+
+def wish(name):
+    print("Hello",name,"Good morning")
+
+wish = x
+wish("Shashi")
+wish("Vishal")
+wish("Kang")
