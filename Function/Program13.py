cd11623a1d08bb9bd467ad4faac53713c79ad21f
Vishal Mahajan <mahajanvishal410a@gmail.com>
2024-01-31 19:47:38 +0530

    Initial commit

---
+++ Function/Program13.py
@@ -0,0 +1,8 @@
+def show(name):
+    print("My name is {}".format(name))
+  
+def show(name):
+    print("My name is not {}".format(name))
+    
+
+show("Raj")
--- Strings/Program05.py
+++ Strings/Program05.py
@@ -2,3 +2,4 @@ a=2+5j
 
 print(f"Value of a={a}")
 print("Value of a=%s"%a)
+print("Value of a={}".format(a))
