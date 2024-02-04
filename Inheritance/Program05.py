f8b74f112a5664c915a6c5660c6d6713ee3278d1
Vishal Mahajan <mahajanvishal410a@gmail.com>
2024-02-04 11:07:18 +0530

    Initial commit

---
+++ Inheritance/Program05.py
@@ -0,0 +1,13 @@
+class Parent1:
+    def __init__(self):
+        print("In Parent1 constructor")
+    
+class Parent2:
+    def __init__(self):
+        self.x=10
+        print("In Parent2 constructor")
+    
+class Child(Parent1,Parent2):
+    pass
+
+Child()
