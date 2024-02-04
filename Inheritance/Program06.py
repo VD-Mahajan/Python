b8bbc3f4609ae46884f531d01fb63b2fe0560d90
Vishal Mahajan <mahajanvishal410a@gmail.com>
2024-02-04 11:09:21 +0530

    Initial commit

--- Inheritance/Program06.py
+++ Inheritance/Program06.py
@@ -6,8 +6,19 @@ class Parent2:
     def __init__(self):
         self.x=10
         print("In Parent2 constructor")
-    
+    def disp(self):
+        print("In Parent2 disp")
+        
 class Child(Parent1,Parent2):
     pass
 
-Child()   #===> In Parent1 constructor
+Child()
+Child().disp()
+
+
+'''
+o/p:
+In Parent1 constructor
+In Parent1 constructor
+In Parent2 disp
+'''
