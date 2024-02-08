554f36a5404b4bc227de843d206b947f86fc3996
Vishal Mahajan <mahajanvishal410a@gmail.com>
2024-02-08 17:18:56 +0530

    Initial commit

---
+++ Inheritance/Single Inheritance/Program02.py
@@ -0,0 +1,8 @@
+class object(object):
+    def __init__(self):
+        print('inherited object class')
+class object:
+    def __init__(self):
+        print('normal object class')
+
+obj = object()
