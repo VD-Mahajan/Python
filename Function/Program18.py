62460582b1b123db98d18e10aeb72c93b64458e0
Vishal Mahajan <mahajanvishal410a@gmail.com>
2024-02-11 22:02:27 +0530

    Initial commit

---
+++ Function/Program18.py
@@ -0,0 +1,23 @@
+class Demo:
+    def __str__(self):
+        return 'str'
+
+    def __repr__(self):
+        return f'Instance of {self.__class__}'
+
+
+print(Demo())           #=====> Instance of <class '__main__.Demo'>
+
+
+'''
+
+class Demo:
+    def __str__(self):
+        return 'str'
+
+    def __repr__(self):
+        return f'Instance of {self.__class__}'
+
+
+print(Demo())       ===> Instance of <class '__main__.Demo'>
+'''
